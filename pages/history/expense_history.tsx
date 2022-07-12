1c6d074a0db2050271cd0905d51bbb57271e61be
ajeya <ajeyabhat.off@gmail.com>
2022-07-12 21:51:51 +0530

    bud

--- pages/history/expense_history.tsx
+++ pages/history/expense_history.tsx
@@ -55,7 +55,7 @@ const ExpenseHistory: NextPage = () => {
         }
         return (
             <div className={styles.totalPrice}>
-                &#8377; {parseFloat(totalPrice).toFixed(2)}
+                &#8377; {totalPrice.toFixed(2)}
             </div>
         );
     };
